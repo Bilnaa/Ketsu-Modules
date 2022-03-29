36ffa237153461eb04542dc671962fec43efb39a
Nabil <43651360+Bilnaa@users.noreply.github.com>
2022-03-29 02:57:18 +0200

    update the info system ui

--- MangaDex/Info/Info.js
+++ MangaDex/Info/Info.js
@@ -69,15 +69,11 @@ function getHtmlStuff(array, match) {
         }
     }
 }
-
 var savedData = document.getElementById('ketsu-final-data');
 var parsedJson = JSON.parse(savedData.innerHTML);
 let emptyKeyValue = [new KeyValue('', '')];
-
 var episodes = [];
-
 var genresArray = [];
-
 var script = document.querySelector('script').innerHTML.replace('/*', '').replace('*/', '');
 var json = JSON.parse(script);
 var genres = json.data.attributes.tags;
@@ -85,27 +81,30 @@ for (genre of genres) {
     genre = genre.attributes.name.en;
     genresArray.push(genre);
 }
-var type = 'empty';
+var type = json.data.attributes.originalLanguage.toUpperCase();
 var statut = json.data.attributes.status;
 var genres = [];
 try {
     var desc = json.data.attributes.depion.en.trim();
 } catch (e) {
     var desc = json.data.attributes.depion.ja;
-    if(desc==null){
+    if (desc == null) {
         desc = '';
     }
 }
 var title = json.data.attributes.title.en;
+if (title == undefined) {
+    title = json.data.attributes.title.ja
+}
 var relationships = json.data.relationships;
-    for (relationship of relationships) {
-        if (relationship.type == 'cover_art') {
-            var coverArt = relationship.attributes.fileName;
-        } 
+for (relationship of relationships) {
+    if (relationship.type == 'cover_art') {
+        var coverArt = relationship.attributes.fileName;
     }
-    var image = 'https://uploads.mangadex.org/covers/' + json.data.id + '/' + coverArt;
-    image = new ModuleRequest(image, 'get', emptyKeyValue, null);
-var nextRequest = `https://api.mangadex.org/manga/a2c1d849-af05-4bbc-b2a7-866ebb10331f/feed?order[chapter]=asc&order[volume]=asc&limit=500&translatedLanguage[]=es`;
+}
+var image = 'https://uploads.mangadex.org/covers/' + json.data.id + '/' + coverArt;
+image = new ModuleRequest(image, 'get', emptyKeyValue, null);
+var nextRequest = `https://api.mangadex.org/manga/${json.data.id}/feed?order[chapter]=asc&order[volume]=asc&limit=500&translatedLanguage[]=en`;
 let infoPageObject = new Info(new ModuleRequest(nextRequest, 'get', emptyKeyValue, null), new Extra([new Commands('', emptyKeyValue)], emptyKeyValue), new JavascriptConfig(false, false, ''), new Output(image, title, parsedJson.request, desc, genresArray, status, 'Manga', type, 'Eps: ' + episodes.length, episodes));
 var finalJson = JSON.stringify(infoPageObject);
 savedData.innerHTML = finalJson;
