bbb67f4cd0deb3ab9710171a3ecb92931005004c
Nabil <43651360+Bilnaa@users.noreply.github.com>
2022-04-25 02:44:27 +0200

    added 9anime

--- Zoro.to/Info/Info.js
+++ Zoro.to/Info/Info.js
@@ -1,227 +1,87 @@
-const DefaultLayouts = {
-    ultraWideFull: 'ultraWideFull',
-    ultraWide: 'ultraWide',
-    wideFull: 'wideFull',
-    wide: 'wide',
-    wideStrechedFull: 'wideStrechedFull',
-    wideStrechedFullDouble: 'WideStrechedFullDouble',
-    wideStreched: 'wideStreched',
-    wideStrechedDouble: 'wideStrechedDouble',
-    wideStrechedFullList: 'wideStrechedFullList',
-    wideStrechedList: 'wideStrechedList',
-    doublets: 'doublets',
-    doubletsDouble: 'doubletsDouble',
-    doubletsFull: 'doubletsFull',
-    doubletsFullDouble: 'doubletsFullDouble',
-    doubletsConstant: 'doubletsConstant',
-    doubletsDoubleConstant: 'doubletsDoubleConstant',
-    doubletsFullConstant: 'doubletsFullConstant',
-    doubletsFullDoubleConstant: 'doubletsFullDoubleConstant',
-    longDoublets: 'longDoublets',
-    longDoubletsDouble: 'longDoubletsDouble',
-    longDoubletsFull: 'longDoubletsFull',
-    longDoubletsFullDouble: 'longDoubletsFullDouble',
-    longDoubletsConstant: 'longDoubletsConstant',
-    longDoubletsDoubleConstant: 'longDoubletsDoubleConstant',
-    longDoubletsFullConstant: 'longDoubletsFullConstant',
-    longDoubletsFullDoubleConstant: 'longDoubletsFullDoubleConstant',
-    triplets: 'triplets',
-    tripletsDouble: 'tripletsDouble',
-    tripletsFull: 'tripletsFull',
-    tripletsFullDouble: 'tripletsFullDouble',
-    tripletsConstant: 'tripletsConstant',
-    tripletsDoubleConstant: 'tripletsDoubleConstant',
-    tripletsFullConstant: 'tripletsFullConstant',
-    tripletsFullDoubleConstant: 'tripletsFullDoubleConstant',
-    longTriplets: 'longTriplets',
-    longTripletsDouble: 'longTripletsDouble',
-    longTripletsFull: 'longTripletsFull',
-    longTripletsFullDouble: 'longTripletsFullDouble',
-    longTripletsConstant: 'longTripletsConstant',
-    longTripletsDoubleConstant: 'longTripletsDoubleConstant',
-    longTripletsFullConstant: 'longTripletsFullConstant',
-    longTripletsFullDoubleConstant: 'longTripletsFullDoubleConstant',
-    none: ''
-};
-const CellDesings = {
-    Special1: 'Special1',
-    Special2: 'Special2',
-    Special3: 'Special3',
-    CELLHelperText: 'CELLHelperText',
-    small1: 'small1',
-    small2: 'small2',
-    normal1: 'normal1',
-    normal2: 'normal2',
-    normal3: 'normal3',
-    normal4: 'normal4',
-    normal5: 'normal5',
-    normal6: 'normal6',
-    normal7: 'normal7',
-    wide1: 'wide1',
-    wide2: 'wide2',
-    wide3: 'wide3',
-    wide4: 'wide4',
-    wide5: 'wide5',
-    wide6: 'wide6',
-    wide7: 'wide7',
-    wide8: 'wide8',
-    wide9: 'wide9',
-    wide10: 'wide10',
-    wide11: 'wide11'
-};
-const Paging = {
-    leading: 'leading',
-    centered: 'centered',
-    none: ''
-};
-const Orientation = {
-    horizontal: 'horizontal',
-    vertical: 'vertical'
-};
+  function Info(request, extra, javascriptConfig, output) {
+      this.request = request;
+      this.extra = extra;
+      this.javascriptConfig = javascriptConfig;
+      this.output = output;
+  }
 
-function Search(request, extra, separator, javascriptConfig, output) {
-    this.request = request;
-    this.extra = extra;
-    this.separator = separator;
-    this.javascriptConfig = javascriptConfig;
-    this.output = output;
-}
+  function ModuleRequest(url, method, headers, httpBody) {
+      this.url = url;
+      this.method = method;
+      this.headers = headers;
+      this.httpBody = httpBody;
+  }
 
-function ModuleRequest(url, method, headers, httpBody) {
-    this.url = url;
-    this.method = method;
-    this.headers = headers;
-    this.httpBody = httpBody;
-}
+  function Extra(commands, extraInfo) {
+      this.commands = commands;
+      this.extraInfo = extraInfo;
+  }
 
-function Extra(commands, extraInfo) {
-    this.commands = commands;
-    this.extraInfo = extraInfo;
-}
+  function Commands(commandName, params) {
+      this.commandName = commandName;
+      this.params = params;
+  }
 
-function Commands(commandName, params) {
-    this.commandName = commandName;
-    this.params = params;
-}
+  function JavascriptConfig(removeJavascript, loadInWebView, javaScript) {
+      this.removeJavascript = removeJavascript;
+      this.loadInWebView = loadInWebView;
+      this.javaScript = javaScript;
+  }
 
-function JavascriptConfig(removeJavascript, loadInWebView, javaScript) {
-    this.removeJavascript = removeJavascript;
-    this.loadInWebView = loadInWebView;
-    this.javaScript = javaScript;
-}
+  function KeyValue(key, value) {
+      this.key = key;
+      this.value = value;
+  }
 
-function KeyValue(key, value) {
-    this.key = key;
-    this.value = value;
-}
+  function Chapter(chapName, link, openInWebView) {
+      this.chapName = chapName;
+      this.link = link;
+      this.openInWebView = openInWebView;
+  }
 
-function Output(cellDesing, orientation, defaultLayout, paging, section, layout, data) {
-    this.cellDesing = cellDesing;
-    this.orientation = orientation;
-    this.defaultLayout = defaultLayout;
-    this.paging = paging;
-    this.section = section;
-    this.layout = layout;
-    this.data = data;
-}
-
-function Section(sectionName, separator) {
-    this.sectionName = sectionName;
-    this.separator = separator;
-}
-
-function Layout(insets, visibleCellsWidthS, visibleCellsWidthM, visibleCellsWidthL, visibleCellsHeight, heightForVisibleCells, cellSize, ratio, constant, horizontalSpacing, verticalSpacing) {
-    this.insets = insets;
-    this.visibleCellsWidthS = visibleCellsWidthS;
-    this.visibleCellsWidthM = visibleCellsWidthM;
-    this.visibleCellsWidthL = visibleCellsWidthL;
-    this.visibleCellsHeight = visibleCellsHeight;
-    this.heightForVisibleCells = heightForVisibleCells;
-    this.cellSize = cellSize;
-    this.ratio = ratio;
-    this.constant = constant;
-    this.horizontalSpacing = horizontalSpacing;
-    this.verticalSpacing = verticalSpacing;
-}
-
-function Insets(top, bottom, left, right) {
-    this.top = top;
-    this.bottom = bottom;
-    this.left = left;
-    this.right = right;
-}
-
-function Size(width, height) {
-    this.width = width;
-    this.height = height;
-}
-
-function Ratio(inRelation, number1, number2) {
-    this.inRelation = inRelation;
-    this.number1 = number1;
-    this.number2 = number2;
-}
-
-function Data(image, title, description, field1, field2, field3, field4, isChapter, link, openInWebView) {
-    this.image = image;
-    this.title = title;
-    this.description = description;
-    this.field1 = field1;
-    this.field2 = field2;
-    this.field3 = field3;
-    this.field4 = field4;
-    this.isChapter = isChapter;
-    this.link = link;
-    this.openInWebView = openInWebView;
-}
-
-function quickData(link, image, title, field1) {
-    return new Data(image, title, 'unknown', field1, 'unknown', 'unknown', 'unknown', false, link, false);
-}
-
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
-}
-var savedData = document.getElementById('ketsu-final-data');
-var parsedJson = JSON.parse(savedData.innerHTML);
-let output = [];
-let emptyKeyValue = [new KeyValue('', '')];
-if (!parsedJson.request.url.split('keyword=')[1].includes('random')) {
-    var nextRequest = '';
-    var searchArray = [];
-    var shorts = document.querySelectorAll('.film_list-wrap .flw-item');
-    for (short of shorts) {
-        var image = short.querySelector('img').dataset.src;
-        image = new ModuleRequest(image, 'get', emptyKeyValue, null);
-        var title = short.querySelector('img').alt;
-        var field1 = '';
-        try {
-            language = short.querySelector('.tick.ltr').textContent.replaceAll('\\n', '').trim().replace(' ', '/').replaceAll(' ', '');
-        } catch {
-            language = short.querySelector('.tick.ltr');
-            if (language == null) {
-                language = '??';
-            } else {
-                language = short.querySelector('.tick.ltr').innerText.trim().replace('\\n', '/');
-            }
-        }
-        var field1 = short.querySelector('.tick-item.tick-eps').textContent.trim();
-        var link = 'https://zoro.to/' + short.querySelector('a').href;
-        link = new ModuleRequest(link, 'get', emptyKeyValue, null);
-        searchArray.push(new Data(image, title, field1 + ' ' + language, '', language, '', '', false, link));
-    }
-    var testLayout = new Layout(new Insets(10, 10, 10, 10), 1, 2, 3, 1, 500, new Size(400, 400), new Ratio('width', 4, 11), new Size(0, 0), 10, 10);
-    output.push(new Output(CellDesings.wide8, Orientation.vertical, DefaultLayouts.none, Paging.none, new Section('', true), testLayout, searchArray));
-} else {
-    nextRequest = 'https://zoro.to/random';
-}
-let searchPageObject = new Search(new ModuleRequest(nextRequest, 'get', emptyKeyValue, null), new Extra([new Commands('', emptyKeyValue)], emptyKeyValue), '', new JavascriptConfig(true, false, ''), output);
-var finalJson = JSON.stringify(searchPageObject);
-savedData.innerHTML = finalJson;
+  function Output(image, title, link, description, genres, field1, field2, field3, field4, chapters) {
+      this.image = image;
+      this.link = link;
+      this.title = title;
+      this.description = description;
+      this.genres = genres;
+      this.field1 = field1;
+      this.field2 = field2;
+      this.field3 = field3;
+      this.field4 = field4;
+      this.chapters = chapters;
+  }
+  var savedData = document.getElementById('ketsu-final-data');
+  var parsedJson = JSON.parse(savedData.innerHTML);
+  let emptyKeyValue = [new KeyValue('Referer', 'https://zoro.to')];
+  var airing;
+  var status;
+  var image = document.querySelector('.film-poster img').src;
+  image = new ModuleRequest(image, 'get', emptyKeyValue, null);
+  if (document.querySelector('.anisc-detail h2').textContent != document.querySelector('.anisc-detail h2').dataset.jname) {
+      var title = document.querySelector('.anisc-detail h2').textContent + '/' + document.querySelector('.anisc-detail h2').dataset.jname
+  } else {
+      var title = document.querySelector('.anisc-detail h2').dataset.jname
+  };
+  var desc = '';
+  try {
+      desc = document.querySelector('div.anis-content > div.anisc-detail > div.film-depion.m-hide > div').textContent.trim();
+  } catch (e) {
+      desc = document.querySelector('.anisc-info').textContent.trim();
+  }
+  var animeinfo = document.querySelector('div.anisc-info-wrap > div.anisc-info');
+  for (var x = 0; x < animeinfo.children.length; x++) {
+      if (animeinfo.children[x].textContent.trim().includes('Status')) {
+          var status = animeinfo.children[x].textContent.split('Status:')[1].trim();
+      } else if (animeinfo.children[x].textContent.trim().includes('Aired')) {
+          var airing = animeinfo.children[x].textContent.split('Aired:')[1].trim();
+      }
+  }
+  var genres = Array.from(document.querySelectorAll('.item.item-list a')).map(g => g.textContent);
+  var type = 'Anime';
+  var episodes = [];
+  var json = document.querySelector('#wrapper').dataset.id;
+  var nextRequest = `https://zoro.to/ajax/v2/episode/list/${json}`;
+  let infoPageObject = new Info(new ModuleRequest(nextRequest, 'get', emptyKeyValue, null), new Extra([new Commands('', emptyKeyValue)], emptyKeyValue), new JavascriptConfig(false, false, ''), new Output(image, title, parsedJson.request, desc, genres, status, airing, type, 'Eps: ' + episodes.length, episodes));
+  var finalJson = JSON.stringify(infoPageObject);
+  savedData.innerHTML = finalJson;
