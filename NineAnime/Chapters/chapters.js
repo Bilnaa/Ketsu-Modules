5d2b3a9082077c60c677215f01367bb5d224c2aa
Bilnaa <noufele.mehdi@hotmail.fr>
2023-01-12 21:24:02 +0100

    [skip ci] updated 9anime

--- NineAnime/Chapters/chapters.js
+++ NineAnime/Chapters/chapters.js
@@ -1,74 +1,120 @@
-function Chapters(request, extra, javascriptConfig, output) {
+function Chapters(request, extra, javascriptConfig, output)
+{
     this.request = request;
     this.extra = extra;
     this.javascriptConfig = javascriptConfig;
     this.output = output;
 }
 
-function ModuleRequest(url, method, headers, httpBody) {
+function ModuleRequest(url, method, headers, httpBody)
+{
     this.url = url;
     this.method = method;
     this.headers = headers;
     this.httpBody = httpBody;
 }
 
-function Extra(commands, extraInfo) {
+function Extra(commands, extraInfo)
+{
     this.commands = commands;
     this.extraInfo = extraInfo;
 }
 
-function Commands(commandName, params) {
+function Commands(commandName, params)
+{
     this.commandName = commandName;
     this.params = params;
 }
 
-function JavascriptConfig(removeJavascript, loadInWebView, javaScript) {
+function JavascriptConfig(removeJavascript, loadInWebView, javaScript)
+{
     this.removeJavascript = removeJavascript;
     this.loadInWebView = loadInWebView;
     this.javaScript = javaScript;
 }
 
-function KeyValue(key, value) {
+function KeyValue(key, value)
+{
     this.key = key;
     this.value = value;
 }
 
-function Output(videos, images, text) {
+function Output(videos, images, text)
+{
     this.videos = videos;
     this.images = images;
     this.text = text;
 }
 
-function Videos(needsResolver, rawVideo) {
+function Videos(needsResolver, rawVideo)
+{
     this.needsResolver = needsResolver;
     this.rawVideo = rawVideo;
 }
 
-function NeedsResolver(resolverIdentifier, link) {
+function NeedsResolver(resolverIdentifier, link)
+{
     this.resolverIdentifier = resolverIdentifier;
     this.link = link;
 }
 
-function RawVideo(video) {
+function RawVideo(video)
+{
     this.video = video;
 }
 
-function Video(videoQuality, videoLink) {
+function Video(videoQuality, videoLink)
+{
     this.videoQuality = videoQuality;
     this.videoLink = videoLink;
 }
 
-function getValueFromKey(keys, key) {
-    for (var x = 0; x < keys.length; x++) {
+function getValueFromKey(keys, key)
+{
+    for (var x = 0; x < keys.length; x++)
+    {
         let tKey = keys[x];
-        if (tKey.key == key) {
+        if (tKey.key == key)
+        {
             return tKey.value;
         }
     }
 }
-async function sleep(ms) {
+async function sleep(ms)
+{
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+class iframeInterval
+{
+    constructor(time, maxTries, callback, maxTriesCallback)
+    {
+        this.tries = 0;
+
+        this.interval = setInterval(async () =>
+        {
+            if (this.tries >= maxTries)
+            {
+                maxTriesCallback(this);
+                this.cancelTimer();
+                return
+            }
+
+            callback(this);
+            this.tries += 1;
+        }, time);
+
+    }
+
+    cancelTimer()
+    {
+        clearInterval(this.interval);
+
+    }
+}
+
+var global = window || global;
+global.iframeInterval = iframeInterval;
 
 var savedData = document.getElementById('ketsu-final-data');
 var parsedJson = JSON.parse(savedData.innerHTML);
@@ -79,38 +125,80 @@ var KETSU_ASYNC = true;
 var output = [];
 
 
-
-async function getVideos() {
-    for (let x = 0; x < 100; x++) {
-        if (output.length >= 1) {
-            break
-        } else {
-            await sleep(1000).then(async () => {
-                let divProv = document.querySelectorAll("#w-servers > div.servers .type li");
-                if (divProv.length >= 1) {
-                    for (let prov of divProv) {
-                        prov.dispatchEvent(new Event('click'));
-                      await sleep(2500).then(async ()=> {
-                        for (let i = 0; i < 100; i++) {
-                            let iframe = document.querySelector('iframe');
-                            if (iframe.src != undefined) {
-                                await sleep(2500).then(() => {
-                                    let link = document.querySelector('iframe').src;
-                                    output.push(new NeedsResolver('', new ModuleRequest(link, 'get', emptyKeyValue, null)));
-                                })
-                                break;
-                            }
-                        }
-                      })
-                    }
-                }
-            })
+new iframeInterval(100, 300, (interval) =>
+    {
+        // Callback
+        let buttons = document.querySelectorAll("#w-servers > div.servers .type li");
+        if (buttons.length > 0)
+        {
+            getIframe();
+            interval.cancelTimer();
         }
-    }
-    let emptyExtra = new Extra([new Commands('', emptyKeyValue)], extraInfo);
-    var chaptersObject = new Chapters(new ModuleRequest('', 'get', emptyKeyValue, null), emptyExtra, new JavascriptConfig(false, false, ''), new Output(new Videos(output, null), null, null));
-    var finalJson = JSON.stringify(chaptersObject);
-    savedData.innerHTML = finalJson;
-    window.webkit.messageHandlers.EXECUTE_KETSU_ASYNC.postMessage('');
+    },
+    (interval) =>
+    {
+        // Error callback
+        window.webkit.messageHandlers.EXECUTE_KETSU_ASYNC.postMessage('');
+        interval.cancelTimer();
+    })
+
+window.mcs = 0;
+window.foundStrms = [];
+
+async function getIframe()
+{
+
+    let buttons = document.querySelectorAll("#w-servers > div.servers .type li");
+    buttons[this.window.mcs].dispatchEvent(new Event('click'));
+    console.log(window.mcs)
+    new iframeInterval(100, 30, (interval) =>
+        {
+            // Callback
+            let iframe = document.querySelector('iframe');
+            if (iframe.src != undefined && !window.foundStrms.includes(iframe.src))
+            {
+                console.log(iframe.src)
+                window.foundStrms.push(iframe.src);
+                let type = buttons[this.window.mcs].parentElement.parentElement.dataset.type.toUpperCase();
+                if (iframe.src != "" && !iframe.src.includes('addthis') && !iframe.src.includes('disqus.com'))
+                {
+                    let link_match = '';
+                    if(!iframe.src.includes('www')){
+                        link_match = iframe.src.split('/')[2].split('.')[0].toUpperCase()
+                    } else {
+                        link_match = iframe.src.split('/')[2].split('.')[1].toUpperCase()
+                    }
+                    output.push(new NeedsResolver(`(${type}) ${link_match}`, new ModuleRequest(iframe.src, 'get', emptyKeyValue, null)));
+
+                }
+                getNextStream();
+                interval.cancelTimer();
+            }
+
+        },
+        (interval) =>
+        {
+            // Error callback
+            getNextStream();
+            interval.cancelTimer();
+        });
+
 }
-getVideos();
+
+function getNextStream()
+{
+    window.mcs += 1;
+
+    let buttons = document.querySelectorAll("#w-servers > div.servers .type li");
+
+    if (window.mcs >= buttons.length - 1)
+    {
+        let emptyExtra = new Extra([new Commands('', emptyKeyValue)], extraInfo);
+        var chaptersObject = new Chapters(new ModuleRequest('', 'get', emptyKeyValue, null), emptyExtra, new JavascriptConfig(false, false, ''), new Output(new Videos(output, null), null, null));
+        var finalJson = JSON.stringify(chaptersObject);
+        savedData.innerHTML = finalJson;
+        window.webkit.messageHandlers.EXECUTE_KETSU_ASYNC.postMessage('');
+        return;
+    }
+    getIframe();
+}
