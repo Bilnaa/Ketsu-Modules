1f5cc2c28c9f21c95b7d1d517277aa9967a8257a
Nabil <noufnouf703@gmail.com>
2023-09-03 22:10:30 +0200

    fixed all the resolvers

--- NineAnime/Resolvers/filemoon/filemoon.js
+++ NineAnime/Resolvers/filemoon/filemoon.js
@@ -50,6 +50,54 @@ function getNext(match, array) {
         }
     }
 }
+
+function unPack (code) {
+	function indent (code) {
+		try {
+		var tabs = 0, old=-1, add='';
+		for(var i=0;i<code.length;i++) {
+			if(code[i].indexOf("{") != -1) tabs++;
+			if(code[i].indexOf("}") != -1) tabs--;
+			
+			if(old != tabs) {
+				old = tabs;
+				add = "";
+				while (old > 0) {
+					add += "\t";
+					old--;
+				}
+				old = tabs;
+			}
+			
+			code[i] = add + code[i];
+		}
+		} finally {
+			tabs = null;
+			old = null;
+			add = null;
+		}
+		return code;
+	}
+    
+    var env = {
+        eval: function (c) {
+            code = c;
+        },
+        window: {},
+        document: {}
+    };
+    
+    eval("with(env) {" + code + "}");
+	
+	code = (code+"").replace(/;/g, ";\n").replace(/{/g, "\n{\n").replace(/}/g, "\n}\n").replace(/\n;\n/g, ";\n").replace(/\n\n/g, "\n");
+	
+    code = code.split("\n");
+    code = indent(code);
+    
+    code = code.join("\n");
+    return code;
+} 
+
 var savedData = document.getElementById('ketsu-final-data');
 var parsedJson = JSON.parse(savedData.innerHTML);
 var emptyKeyValue = [new KeyValue('Referer', 'https://filemoon.to/'),
@@ -67,15 +115,12 @@ for (let script of scripts) {
     }
 }
 
-let regex = /eval(?<eval>.+)\)/g;
-let regRes = regex.exec(found).groups.eval + ')';
-console.log(regRes)
-let data = eval(regRes);
-console.log(data);
-
-regex = /player\.setup\((?<sources>.+?)\)/g;
+let data = unPack(found);
+data = data.replaceAll('\n','').replaceAll('\t','');
+regex = /videop\.setup\((?<sources>.+?)}\)/g;
 regRes = regex.exec(data).groups.sources;
-console.log(regRes)
+regRes = regRes.replaceAll('\"','"').replace('loader:engine.createLoaderClass()',"loader:null");
+regRes = regRes + '}';
 eval("var parsed = " + regRes)
 console.log(parsed)
 
@@ -87,4 +132,4 @@ for (let source of parsed.sources) {
 let emptyExtra = new Extra([new Commands('', emptyKeyValue)], emptyKeyValue);
 var chaptersObject = new Resolver(new ModuleRequest('', '', emptyKeyValue, null), emptyExtra, new JavascriptConfig(false, false, ''), new Output(videos));
 var finalJson = JSON.stringify(chaptersObject);
-savedData.innerHTML = finalJson;
+savedData.innerHTML = finalJson;       
